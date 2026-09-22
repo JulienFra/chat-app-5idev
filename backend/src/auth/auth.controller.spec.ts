6bff95fd37d4aee76dad9b458157fc206c271437
Florentin28 <muraillef@gmail.com>
2026-09-22 09:24:05 +0200

    fix PR problem

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +8,8 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      // Injection d'un faux service pour satisfaire les dépendances
+      providers: [{ provide: AuthService, useValue: {} }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
